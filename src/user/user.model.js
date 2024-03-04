792a2a17d35b2fdc3314c2a94bd88a04647c51fa
iiboy-2022099 <iiboy-2022099@kinal.edu.gt>
2024-03-04 08:29:23 -0600

    Subiendo el modelo de user

---
+++ src/user/user.model.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "The name is required"],
+    },
+    email: {
+        type: String,
+        required: [true, "The email is required"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ["ADMIN_ROLE",]
+    },
+    state: {
+        type: Boolean,
+        default: true,
+    }
+});
+
+UserSchema.methods.toJSON = function () {
+    const { __v, password, _id, ...user } = this.toObject();
+    user.uid = _id;
+    return user;
+}
+
+export default mongoose.model('User', UserSchema);
